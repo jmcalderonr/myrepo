1a98791086961434d0838ce902595d884da71911
Johan M. Calderon <jmcalderonr83@gmail.com>
2023-04-17 08:31:42 -0500

    Render Script

---
+++ Example.R
@@ -0,0 +1,15 @@
+#'---
+#'title: "Example"
+#'author: "JMC"
+#'date: "2023-04-14"
+#'output: github_document
+#'---
+
+#'Hello my people
+#+
+library(MASS)
+data(Pima.tr)
+data("birthwt")
+birthwt
+
+
